187672621321020a5212b1207d2aa7e899c68e2f
Jake Laurie <jake.gordon.laurie@gmail.com>
2023-01-21 13:19:57 +0800

    single dir proxy

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -3,24 +3,23 @@ use std::io::{Read, Write};
 use std::str::from_utf8;
 
 fn main() {
-    match TcpStream::connect("kiowa.xtenduke.com:2302") {
+    match TcpStream::connect("localhost:8000") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3001");
 
-            let msg = b"Hello!";
+            // valid http/1.1 request
+            let msg = b"GET /index.html HTTP/1.1
+
+                ";
 
             stream.write(msg).unwrap();
             println!("Sent Hello, awaiting reply...");
 
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
+            let mut data = [0 as u8; 1024]; // using 1024 byte buffer
+            match stream.read(&mut data) {
                 Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
+                    let text = from_utf8(&data).unwrap();
+                    println!("got response: {}", text);
                 },
                 Err(e) => {
                     println!("Failed to receive data: {}", e);
--- producer/src/main.rs
+++ producer/src/main.rs
@@ -1,17 +1,44 @@
+use std::str::from_utf8;
 use std::thread;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut client_stream: TcpStream) {
     let mut buffer = [0 as u8; 1024]; // using 1024 byte buffer
     loop {
-        let n = stream.read(&mut buffer).expect("Socket read failed");
+        let n = client_stream.read(&mut buffer).expect("Socket read failed");
+
+        // open a tcp connection to the target - send data back over original TCP connection
+        match TcpStream::connect("localhost:8000") {
+            Ok(mut target_stream) => {
+                println!("Connected to target");
+                // forward the original request to the target
+                target_stream.write(&buffer[0..n]).unwrap();
+                let client_request = from_utf8(&buffer).unwrap();
+                println!("Forwarded request to target: {}", client_request);
+                let mut data = [0 as u8; 1024]; // 1024 byte buff
+                match target_stream.read(&mut data) {
+                    Ok(_) => {
+                        let target_response = from_utf8(&data).unwrap();
+                        println!("Received target data: {}", target_response);
+                        // return the response from the target back to the original client
+                        client_stream.write_all(&data[0..n]).expect("Socket write failed");
+                        println!("Write data to client stream");
+                    },
+                    Err(e) => {
+                        println!("Target failed: {}", e);
+                    }
+                }
+            },
+            Err(e) => {
+                println!("Target connection failed: {}", e);
+            }
+        }
+
         if n == 0 {
             println!("Done");
             return;
         }
-
-        stream.write_all(&buffer[0..n]).expect("Socket write failed");
    }
 }
 
