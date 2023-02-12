4fb71e3270ed33bf28265c0315a932409c5858b8
Jake Laurie <jake.gordon.laurie@gmail.com>
2023-02-12 16:53:05 +0800

    Revert to new connection every loop

--- producer/src/main.rs
+++ producer/src/main.rs
@@ -5,22 +5,31 @@ use std::io::{Read, Write};
 
 fn handle_client(mut client_stream: TcpStream) {
     let mut request_buffer = [0 as u8; 1024];
-    let mut target_stream = TcpStream::connect("localhost:3000").expect("Failed connecting to dest");
     loop {
         let n = client_stream.read(&mut request_buffer).expect("Socket read failed");
-        println!("Connected to target");
-        // forward the original request to the target
-        target_stream.write(&request_buffer[0..n]).unwrap();
-        let client_request = from_utf8(&request_buffer).unwrap();
-        println!("Forwarded request to target: {}", client_request);
-        let mut data = Vec::new();
-        match target_stream.read_to_end(&mut data) {
-            Ok(_) => {
-                client_stream.write_all(&data).expect("TODO: panic message");
-                println!("Write data to client stream, size {} bytes", data.len());
+
+        // open a tcp connection to the target - send data back over original TCP connection
+        // reuse the connection?
+        match TcpStream::connect("localhost:3000") {
+            Ok(mut target_stream) => {
+                println!("Connected to target");
+                // forward the original request to the target
+                target_stream.write(&request_buffer[0..n]).unwrap();
+                // let client_request = from_utf8(&request_buffer).unwrap();
+                // println!("Forwarded request to target: {}", client_request);
+                let mut data = Vec::new();
+                match target_stream.read_to_end(&mut data) {
+                    Ok(_) => {
+                        client_stream.write_all(&data).expect("TODO: panic message");
+                        println!("Write data to client stream, size {} bytes", data.len());
+                    },
+                    Err(e) => {
+                        println!("Target failed: {}", e);
+                    }
+                }
             },
             Err(e) => {
-                println!("Target failed: {}", e);
+                println!("Target connection failed: {}", e);
             }
         }
 
@@ -41,7 +50,6 @@ fn main() {
                 println!("New connection: {}", stream.peer_addr().unwrap());
                 thread::spawn(move|| {
                     // connection succeeded
-                    // need to reference these threads?
                     handle_client(stream)
                 });
             }
